9649abe8f59d588c90b553d249929fb8162766d3
guizaramellaf91 <guizaramellaf91@gmail.com>
2021-06-23 23:11:21 -0300

    up

---
+++ ztech-auth-server/src/main/java/br/com/zaratech/ztech/ZtechAuthServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.zaratech.ztech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtechAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtechAuthServerApplication.class, args);
+	}
+
+}
