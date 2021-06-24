faaaba6d71fe19c5dc17f0753483eef845add73f
guizaramellaf91 <guizaramellaf91@gmail.com>
2021-06-23 23:32:45 -0300

    new configs

--- ztech-auth-server/src/main/java/br/com/zaratech/ztech/domain/Authorities.java
+++ ztech-auth-server/src/main/java/br/com/zaratech/ztech/domain/Authorities.java
@@ -1,5 +1,15 @@
 package br.com.zaratech.ztech.domain;
 
-public class Authorities {
+public enum Authorities {
 
+    ROLE_USER,
+    ROLE_ADMIN;
+
+    public static String[] names() {
+    	String[] names = new String[values().length];
+    	for(int index = 0; index < values().length; index++) {
+    		names[index] = values()[index].name();
+    	}	
+    	return names;
+    }
 }
