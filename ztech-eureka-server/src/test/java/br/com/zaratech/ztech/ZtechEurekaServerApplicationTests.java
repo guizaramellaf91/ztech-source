1c2e2e3231e726abd3d6a0de0c968433fa1a752c
guizaramellaf91 <guizaramellaf91@gmail.com>
2021-06-23 22:14:17 -0300

    new ms

---
+++ ztech-eureka-server/src/test/java/br/com/zaratech/ztech/ZtechEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zaratech.ztech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtechEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
